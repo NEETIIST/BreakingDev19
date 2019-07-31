9eaafb245af57d44a7336b81a4502e82ec4c22c9
Ubuntu <ubuntu@neetiserver.neeti.tp.vps.tecnico.ulisboa.pt>
2019-07-31 17:05:43 +0000

    Updates

---
+++ precache-manifest.184807f5b88e749c03d90823694c625b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d0a0c2df2652a28e9c81",
+    "url": "/static/js/main.9c1bcd1c.chunk.js"
+  },
+  {
+    "revision": "6d7a980e6f43cd0c735e",
+    "url": "/static/js/2.46c37200.chunk.js"
+  },
+  {
+    "revision": "d0a0c2df2652a28e9c81",
+    "url": "/static/css/main.d10f90ed.chunk.css"
+  },
+  {
+    "revision": "6d7a980e6f43cd0c735e",
+    "url": "/static/css/2.f568b7fb.chunk.css"
+  },
+  {
+    "revision": "25b682cc85387deabe0578ed1c0ec54d",
+    "url": "/index.html"
+  }
+];
