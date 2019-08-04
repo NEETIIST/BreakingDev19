7b727077c682aea79fd04da74e0e81c6a75d81fb
Filipe Ramalho Fernandes <filipe.ramalhofernandes@gmail.com>
2019-08-04 03:04:06 +0100

    Bugfix

--- bdev-frontend/build_run.sh
+++ bdev-frontend/build_run.sh
@@ -3,4 +3,4 @@
 npm install
 npm run build
 #serve -s build -l 3000 &
-nohup ./start_serve.sh > /dev/null 2>&1 & echo $! > ~/pidfile
+((serve -s build -l 3000) & echo $! > /home/ubuntu/breakingdev19/pidfile &)
